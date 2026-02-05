638970b9b7ab82ca0737f6f1a36eab509d10962b
Lloyd Lentz <cledwyn@gmail.com>
2026-02-04 23:07:45 -0500

    Diagnostic: check advisor for specific student

--- queries/list-portal-methods.sql
+++ queries/list-portal-methods.sql
@@ -1,21 +1,22 @@
--- Diagnostic: Explore Advisor entity structure
--- Looking at how advisor_person field links to person records
+-- Diagnostic: Check advisor for specific student (Ahmed Abdelhai)
+-- Student ID: 52ede6c5-7212-4db1-87b4-93f6fb1cb5d2
 
-SELECT TOP 10
+SELECT
     p.id AS student_id,
     p.first AS student_first,
     p.last AS student_last,
     adv_entity.id AS advisor_entity_id,
+    adv_entity.entity AS entity_guid,
     adv_field.field AS field_name,
     adv_field.value AS field_value,
     adv_field.related AS field_related,
+    adv_person.id AS advisor_id,
     adv_person.first AS advisor_first,
     adv_person.last AS advisor_last
 FROM [person] p
-INNER JOIN [entity] adv_entity ON adv_entity.record = p.id
+LEFT JOIN [entity] adv_entity ON adv_entity.record = p.id
     AND adv_entity.entity = '06d6334d-392f-4686-aaa1-ddd2e5640c2b'
-INNER JOIN [field] adv_field ON adv_field.record = adv_entity.id
+LEFT JOIN [field] adv_field ON adv_field.record = adv_entity.id
     AND adv_field.field = 'advisor_person'
-LEFT JOIN [person] adv_person ON adv_person.id = adv_field.value
-LEFT JOIN [person] adv_person2 ON adv_person2.id = adv_field.related
-ORDER BY p.last, p.first
+LEFT JOIN [person] adv_person ON adv_person.id = adv_field.related
+WHERE p.id = '52ede6c5-7212-4db1-87b4-93f6fb1cb5d2'
