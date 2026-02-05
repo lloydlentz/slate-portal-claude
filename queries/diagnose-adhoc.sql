0c46530a186d373c1815eda77311c9903cd5e828
Lloyd Lentz <cledwyn@gmail.com>
2026-02-04 22:32:13 -0600

    Update diagnose-adhoc.sql

--- queries/diagnose-adhoc.sql
+++ queries/diagnose-adhoc.sql
@@ -3,3 +3,4 @@
 SELECT 'person' AS found_in, id, first, last FROM [person] WHERE id = '639b337f-1768-4788-94f9-19d4c0c134fc'
 UNION ALL
 SELECT 'user' AS found_in, id, first, last FROM [user] WHERE id = '639b337f-1768-4788-94f9-19d4c0c134fc'
+
