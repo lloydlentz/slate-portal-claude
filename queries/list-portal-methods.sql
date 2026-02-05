07a77c65f52e0477b28007c022b6275a4114afa2
Lloyd Lentz <cledwyn@gmail.com>
2026-02-04 23:08:47 -0500

    Diagnostic: show raw field_related and test subquery

--- queries/list-portal-methods.sql
+++ queries/list-portal-methods.sql
@@ -1,22 +1,15 @@
--- Diagnostic: Check advisor for specific student (Ahmed Abdelhai)
--- Student ID: 52ede6c5-7212-4db1-87b4-93f6fb1cb5d2
+-- Diagnostic: Show raw field_related value for advisor
+-- Check if it's a valid person GUID
 
-SELECT
-    p.id AS student_id,
-    p.first AS student_first,
-    p.last AS student_last,
-    adv_entity.id AS advisor_entity_id,
-    adv_entity.entity AS entity_guid,
+SELECT TOP 5
+    p.first + ' ' + p.last AS student_name,
     adv_field.field AS field_name,
-    adv_field.value AS field_value,
-    adv_field.related AS field_related,
-    adv_person.id AS advisor_id,
-    adv_person.first AS advisor_first,
-    adv_person.last AS advisor_last
+    adv_field.related AS raw_related,
+    CAST(adv_field.related AS NVARCHAR(100)) AS related_as_string,
+    (SELECT p2.first + ' ' + p2.last FROM [person] p2 WHERE p2.id = adv_field.related) AS advisor_name_subquery
 FROM [person] p
-LEFT JOIN [entity] adv_entity ON adv_entity.record = p.id
+INNER JOIN [entity] adv_entity ON adv_entity.record = p.id
     AND adv_entity.entity = '06d6334d-392f-4686-aaa1-ddd2e5640c2b'
-LEFT JOIN [field] adv_field ON adv_field.record = adv_entity.id
+INNER JOIN [field] adv_field ON adv_field.record = adv_entity.id
     AND adv_field.field = 'advisor_person'
-LEFT JOIN [person] adv_person ON adv_person.id = adv_field.related
-WHERE p.id = '52ede6c5-7212-4db1-87b4-93f6fb1cb5d2'
+WHERE adv_field.related IS NOT NULL
